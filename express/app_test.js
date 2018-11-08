a9ea33e4ee2538598112b5d93a4bd2808a0b3c51
Jae Jang <jaejang5566@gmail.com>
2018-11-07 17:59:37 -0800

    express generator, post, view engine' ;

--- express/app_test.js
+++ express/app_test.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
 
 var app = express();
+
+app.set('views',path.join(__dirname, 'views'));
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({ extended : false}));
+
 app.get('/', (req, res)=>{
 	res.send('Hello  home page');
 });
-
-app.get('/login', (req, res)=>{
-	res.send('Login please');
-});
-
 
 app.get('/dynamic', (req, res)=>{
 	fs.readFile('public/dynamic.html','utf8', (err, data)=>{
@@ -17,37 +22,33 @@ app.get('/dynamic', (req, res)=>{
 	});
 });
 
-app.get('/topic', (req, res)=>{
-	let topics = [
-		'JavaScript is ...',
-		'NodeJs is ...',
-		'Express is ...'
-	];
+app.get('/form', (req, res)=>{
+	res.render('form');
+});
 
+app.post('/form_result', (req, res)=>{
+	const name = req.body.name;
+	const id = req.body.id;
+	res.send(name + ', ' + id);
+});
+
+var topics = [
+	'JavaScript is ...',
+	'NodeJs is ...',
+	'Express is ...'
+];
+
+app.get('/topic4/:id',(req,res)=>{
+	res.render('index',{topic:topics[req.params.id]});
+});
+
+app.get('/topic', (req, res)=>{
 	let output = `
 		<a href="/topic?id=0">JavaScript</a><br>
 		<a href="/topic?id=1">Nodejs</a><br>
 		<a href="/topic?id=2">Express</a><br>
 		${topics[req.query.id]}
 	`;
-	res.send(output);
-});
-
-app.get('/topic2/:id', (req, res)=>{
-	let topics = [
-		'JavaScript is ...',
-		'NodeJs is ...',
-		'Express is ...'
-	];
-
-	let output = `
-		<a href="/topic?id=0">JavaScript</a><br>
-		<a href="/topic?id=1">Nodejs</a><br>
-		<a href="/topic?id=2">Express</a><br>`
-		if (topics[req.params.id])
-			output += topics[req.params.id]
-		//${topics[req.params.id]}
-	;
 	res.send(output);
 });
 
